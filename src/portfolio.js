84018352fa6ec3b0e9ce54789817959175134bb3
KIRA MASTER <47713860+Kira-Master@users.noreply.github.com>
2024-07-30 10:50:58 +0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://fauzinurrokhim.my.id',
+  title: 'FAUZI.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
+  name: 'Fauzi Nurrokhim',
   role: 'Front End Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'Just someone who loves the world of coding.',
+  resume: 'https://fauzinurrokhim.my.id/cv.pdf',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://linkedin.com/fauzinurrokhim',
+    github: 'https://github.com/Kira-Master',
   },
 }
 
@@ -21,28 +21,28 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'BOT Whatsapp Multi Function',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Creating a Multi-Function WhatsApp BOT,using Librady Baileys',
+    stack: 'NodeJS',
+    sourceCode: 'https://github.com/Kira-Master/WhatsappBot-Zexxa',
+    livePreview: 'https://github.com/Kira-Master/WhatsappBot-Zexxa',
   },
   {
-    name: 'Project 2',
+    name: 'Restful-API Website',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Creating a Restful-API website to fulfill the features and needs of my Whatsapp BOT',
+    stack: ['ExpressJS', 'NodeJS'],
+    sourceCode: 'https://github.com/Kira-Master/MyFirstExpress',
+    livePreview: 'https://web.fauzinurrokhim.my.id/',
   },
   {
-    name: 'Project 3',
+    name: 'Personal Portfolio',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Create a personal portfolio using ReactJS',
+    stack: ['SASS', 'React'],
+    sourceCode: 'https://github.com/Kira-Master',
+    livePreview: '/',
   },
 ]
 
@@ -54,18 +54,14 @@ const skills = [
   'JavaScript',
   'TypeScript',
   'React',
-  'Redux',
-  'SASS',
   'Material UI',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'MS Office',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'fauzinurokhim1@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
